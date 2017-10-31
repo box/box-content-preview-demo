80d67c3332a304dfab3d33a1413f5f6033fa6c2a
Priyajeet Hora <priyajeet@box.com>
2017-10-31 11:36:31 -0700

    Chore: Upgrade to latest versions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { ContentPreview } from 'box-ui-elements';
+import ContentPreview from 'box-ui-elements/lib/components/ContentPreview';
+import messages from 'box-ui-elements/lib/i18n/en-US';
+import 'box-ui-elements/dist/explorer.css';
 import './index.scss';
 
 const FILE_ID_DOC = '93392244621';
@@ -12,10 +14,15 @@ const FILE_ID_EXCEL = '173493670106';
 const container = document.querySelector('.container');
 const token = 'EqFyi1Yq1tD9mxY8F38sxDfp73pFd7FP';
 
+const getLocalizedMessage = (id, replacements) =>
+    messages[id].replace(/{\s*(\w+)\s*}/g, (match, key) => replacements[key]);
+
+
 render(
     <ContentPreview
         fileId={FILE_ID_DOC}
         token={token}
+        getLocalizedMessage={getLocalizedMessage}
         collection={[FILE_ID_DOC, FILE_ID_EXCEL, FILE_ID_VIDEO, FILE_ID_3D, FILE_ID_TEXT]}
     />,
     container
